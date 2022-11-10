bdb5f76b73a8d6ec8d6c5c176bed33e2775ace2c
Rodel Bosque <rgbosque@gmail.com>
2022-11-11 01:10:27 +0800

    update comment notes and add company info

--- comment.py
+++ comment.py
@@ -1,8 +1,12 @@
+# company name: MariaClara Solutions
+# developer: Rodel Bosque
+# data: Nov. 11, 2022
+
 import os
 
 os.system("clear")
 
-# This will print 
+# This will not print 
 # Hello World on screen
 print("Hello World!")
 print("I'm using comment on my code")
